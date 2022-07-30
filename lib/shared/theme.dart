5ba288c3fb3d5f2341869e1b5d5cadab928fe7a5
anggaaprianda99 <anggaaprianda04@gmail.com>
2022-07-31 02:32:37 +0700

    add shared and google font

---
+++ lib/shared/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0xff5C40CC);
+Color kBlackColor = const Color(0xff1F1449);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xff0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+TextStyle blackTextStyle = GoogleFonts.poppins(color: kBlackColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: kGreenColor);
+TextStyle redTextStyle = GoogleFonts.poppins(color: kRedColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
