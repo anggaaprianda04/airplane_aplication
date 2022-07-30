f6c780017db7dac77017fd34cac54da254a5a22c
anggaaprianda99 <anggaaprianda04@gmail.com>
2022-07-31 03:17:08 +0700

    add splash page

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -6,12 +6,14 @@ Color kBlackColor = const Color(0xff1F1449);
 Color kGreyColor = const Color(0xff9698A9);
 Color kGreenColor = const Color(0xff0EC3AE);
 Color kRedColor = const Color(0xffEB70A5);
+Color kWhiteColor = const Color(0xffFFFFFF);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
 TextStyle blackTextStyle = GoogleFonts.poppins(color: kBlackColor);
 TextStyle greyTextStyle = GoogleFonts.poppins(color: kGreyColor);
 TextStyle greenTextStyle = GoogleFonts.poppins(color: kGreenColor);
 TextStyle redTextStyle = GoogleFonts.poppins(color: kRedColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: kWhiteColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:airplane_aplication/shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.only(bottom: 30),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_plane.png'),
+                ),
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                  fontSize: 32, letterSpacing: 10, fontWeight: medium),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
